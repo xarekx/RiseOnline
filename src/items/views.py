03a64d5aa8450730b8462273cca5cf2f9a41650a
Arkadiusz Tarnowski <arektarnowski4@gmail.com>
2023-01-10 12:41:55 +0100

    delete items endpoint, delete items template

--- src/items/views.py
+++ src/items/views.py
@@ -3,11 +3,6 @@ from menu.models import LeftSideMenu
 from items.models import Items
 
 # Create your views here.
-def items(request):
-    left_side_item = LeftSideMenu.objects.all()
-    
-    return render(request, 'menu/items.html', { 'left_side_item': left_side_item})
-
 
 def weapons(request):
     left_side_item = LeftSideMenu.objects.all()
