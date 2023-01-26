fce8691fb890df05d8c94543bb9f399d3caa7b95
Arkadiusz Tarnowski <arektarnowski4@gmail.com>
2023-01-26 13:55:54 +0100

    add Meta data to article model

--- src/articles/models.py
+++ src/articles/models.py
@@ -5,10 +5,16 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Article(models.Model):
-     title = models.CharField(_("title"), max_length=64)
-     article = models.TextField(_("article"))
-     created_date = models.DateField(_("created_date"), auto_now=True)
-     created_by = models.ForeignKey(User, related_name="article_created_by", on_delete=models.CASCADE)
+    title = models.CharField(_("title"), max_length=64)
+    article = models.TextField(_("article"))
+    created_date = models.DateField(_("created_date"), auto_now=True)
+    created_by = models.ForeignKey(User, related_name="article_created_by", on_delete=models.CASCADE)
+     
+    class Meta:
+        db_table = "rise_article"
+        verbose_name = _("article")
+        verbose_name_plural = _('articles')
+         
 
-     def __str__(self):
+    def __str__(self):
          return self.title
