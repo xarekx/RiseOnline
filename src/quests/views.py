37e979db489095da661fcdce02763b36a4771d0d
Arkadiusz Tarnowski <arektarnowski4@gmail.com>
2023-01-27 10:34:37 +0100

    rename Quest model, added meta class to model

--- src/quests/views.py
+++ src/quests/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from quests.models import Quests
+from quests.models import Quest
 from rewards.models import Rewards
 
 from menu.models import LeftSideMenu
 
 # Create your views here.
 def quests(request):
-    quest_list = Quests.objects.all().order_by('required_level')
+    quest_list = Quest.objects.all().order_by('required_level')
     left_side_item = LeftSideMenu.objects.all()
     rewards = Rewards.objects.all()
     
--- src/rewards/models.py
+++ src/rewards/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
-from quests.models import Quests
+from quests.models import Quest
 
 # Create your models here.
 
@@ -19,7 +19,7 @@ class Rewards(models.Model):
     )
     
     name = models.CharField(_("name"), max_length=64)
-    quests = models.ForeignKey(Quests, on_delete=models.CASCADE, default=1)
+    quests = models.ForeignKey(Quest, on_delete=models.CASCADE, default=1)
     count = models.PositiveBigIntegerField(_("count"), default=0)
     upgrade_level = models.PositiveSmallIntegerField(_("upgrade_level"), null=True)
     reward_type = models.CharField(_("reward_type"), max_length=16, default="", choices=type_choices)
