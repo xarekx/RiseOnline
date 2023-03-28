ddb8ddd5fcb47d41349507584c9fc00d26a549a1
Arkadiusz Tarnowski <arektarnowski4@gmail.com>
2023-03-28 14:33:57 +0200

    Added pagination to quest template with sort option on entire quest list

--- src/quests/views.py
+++ src/quests/views.py
@@ -7,14 +7,33 @@ from menu.models import LeftSideMenu
 
 # Create your views here.
 def quests(request):
-    quest_list = Quest.objects.all().order_by('required_level')
-    left_side_item = LeftSideMenu.objects.all().order_by("priority")
-    rewards = Reward.objects.all()
-    # pagination on the quest template
-    # paginator = Paginator(quest_list,25)
-    # page_number = request.GET.get('page')
-    # page_obj = paginator.get_page(page_number)
     
-    return render(request, 'menu/quests.html', {'left_side_item': left_side_item
-                                                , 'quests': quest_list, 'rewards':rewards})
+    left_side_item = LeftSideMenu.objects.all().order_by("priority")
+
+    rewards = Reward.objects.all()
+    
+    order_by = request.GET.get('order_by')
+    direction = request.GET.get('direction')
+    if order_by is None or order_by == 'required_level':
+        ordering = 'required_level'
+    elif order_by  == 'quest_zone':
+            ordering = 'quest_zone'  
+    elif order_by  == 'quest_name':
+            ordering = 'quest_name' 
+    if direction == 'desc':
+        ordering = '-{}'.format(ordering)
+        
+    quest_list = Quest.objects.all().order_by(ordering)
+    
+    # pagination on the quest template
+    paginator = Paginator(quest_list,20)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    
+    
+    return render(request, 'menu/quests.html', {'left_side_item': left_side_item,
+                                                'rewards':rewards, 
+                                                'direction':direction, 
+                                                'quests': page_obj, 
+                                                'order_by':order_by})
     
