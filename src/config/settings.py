74fe586a6d5b116c548180dc6582739947050cfa
Arkadiusz Tarnowski <arektarnowski4@gmail.com>
2023-06-15 13:59:08 +0200

    create breadcrumb navigation in base template, navigation is extended to every template

--- src/config/settings.py
+++ src/config/settings.py
@@ -81,6 +81,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'config.context_processors.get_site_url',
             ],
         },
     },
